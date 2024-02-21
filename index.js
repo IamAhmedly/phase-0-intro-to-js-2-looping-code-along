92e0b7800d81a7418f480d6a257e2694493fce0a
Ahmedly SAM <ahmed.esam@duc.edu.iq>
2024-02-21 23:37:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(namesArray) {
+    let messagesArray = [];
+    for (let name of namesArray) {
+      messagesArray.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messagesArray;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
